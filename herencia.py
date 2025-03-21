74da09079047bb73140c951d6b692be7928a6293
Marianasofid <marianadelgado@colegioguanenta.edu.co>
2025-03-21 09:32:10 -0500

    actualizacion

--- herencia.py
+++ herencia.py
@@ -4,9 +4,9 @@ class Persona:
     
     # método constructor
     def __init__(self):
-        self.__Nombre = ""
-        self.__Apellidos = ""
-        self.__Edad = 0
+        self.__Nombre = "Mariana"
+        self.__Apellidos = "Delgado"
+        self.__Edad = 14
 
     def getNombre(self):
         return self.__Nombre
@@ -58,17 +58,60 @@ class Alumno(Persona):
         print("\tMatrículas: ", self.__Asignaturas)
 
 class Profesor(Persona):
-    pass
+    def __init__(self):
+        self.__antiguedad = 0
+        self.__tutorias = ""
+        self.__telefono = 0
+
+    def getAntiguedad(self):
+        return self.__antiguedad
+    
+    def setAntiguedad(self, antiguedad):
+        self.__antiguedad = antiguedad
+
+    def getTutorias(self):
+        return self.__tutorias
+    
+    def setTutorias(self, tutorias):
+        self.__tutorias = tutorias
+    
+    def setTelefono(self, telefono):
+        self.__telefono = telefono
+
+    def getTelefono(self):
+        return self.__telefono
+    
+    def mostrarProfesor(self):
+        print("Profesor")
+        print("\tNombre: ", self.getNombre())
+        print("\tApellidos: ", self.getApellidos())
+        print("\tEdad: ", self.getEdad())
+        print("\tAntiguedad: ", self.__antiguedad)
+        print("\tTutorias: ", self.__tutorias)
+
+
+        
 
 # metodo principal
 def main():
     alumno = Alumno()
-    alumno.setNombre("Néstor")
-    alumno.setApellidos("Páez Sarmiento")
-    alumno.setEdad(25)
+    alumno.setNombre("Mariana")
+    alumno.setApellidos("Delgado Solano")
+    alumno.setEdad(14)
     alumno.setCurso("Bachillerato")
-    alumno.setAsignaturas(["Matemáticas", "Tecnología", "Inglés"])
+    alumno.setAsignaturas(["Matematicas", "Tecnologia", "Ingles"])
     alumno.mostrarAlumno()
+
+    # profesor
+
+    profesor = Profesor()
+    profesor.setNombre("Nestor")
+    profesor.setApellidos("Paez Sarmiento")
+    profesor.setEdad(30)
+    profesor.setAntiguedad(15)
+    profesor.setTutorias(["Sistemas 1"])
+    profesor.setTelefono("222 222 2222")
+    profesor.mostrarProfesor()
 
 if __name__ == "__main__":
     main()
